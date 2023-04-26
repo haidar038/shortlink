34bc93c23e54f0182d3ba8aaa8925ccb35c14e70
M. Khaidar <62201903+haidar038@users.noreply.github.com>
2023-04-27 00:04:25 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
+function copyToClipboard() {
+    // Select the shortened URL field
+    var shortenedUrlField = document.getElementById("shortened-url");
+    shortenedUrlField.select();
+
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+
+    // Show a toast message indicating the URL has been copied
+    var toast = document.getElementById("copy-toast");
+    toast.classList.add("show");
+    setTimeout(function () {
+        toast.classList.remove("show");
+    }, 3000);
+}
+
 function onScroll(evt) {
     // Store the scroll value for laterz.
     lastScrollY = window.scrollY;
@@ -10,3 +26,7 @@ function onScroll(evt) {
 }
 
 window.addEventListener("scroll", onScroll);
+
+// Tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
