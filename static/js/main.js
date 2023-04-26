9562022ccb260e01cd1392d27dc0547ea5aa1517
M. Khaidar <62201903+haidar038@users.noreply.github.com>
2023-04-27 07:23:40 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,1 @@
-function copyToClipboard() {
-    document.getElementById("shortened-url").select(), document.execCommand("copy");
-    var o = document.getElementById("copy-toast");
-    o.classList.add("show"),
-        setTimeout(function () {
-            o.classList.remove("show");
-        }, 3e3);
-}
-function onScroll(o) {
-    (lastScrollY = window.scrollY), scheduledAnimationFrame || ((scheduledAnimationFrame = !0), requestAnimationFrame(readAndUpdatePage));
-}
-window.addEventListener("scroll", onScroll);
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]'),
-    tooltipList = [...tooltipTriggerList].map((o) => new bootstrap.Tooltip(o));
+function copyToClipboard(){document.getElementById("shortened-url").select(),document.execCommand("copy");var o=document.getElementById("copy-toast");o.classList.add("show"),setTimeout(function(){o.classList.remove("show")},3e3)}function onScroll(o){lastScrollY=window.scrollY,scheduledAnimationFrame||(scheduledAnimationFrame=!0,requestAnimationFrame(readAndUpdatePage))}window.addEventListener("scroll",onScroll);const tooltipTriggerList=document.querySelectorAll('[data-bs-toggle="tooltip"]'),tooltipList=[...tooltipTriggerList].map(o=>new bootstrap.Tooltip(o));
